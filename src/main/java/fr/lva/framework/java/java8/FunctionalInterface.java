5a368f32ad007fcbe83605d8afaf506c210a7548
Loic Vasseur <loic@share-d.com>
2022-01-05 11:32:06 +0100

    Add filter by unique attribute predicate

--- src/main/java/fr/lva/framework/java/java8/FunctionalInterface.java
+++ src/main/java/fr/lva/framework/java/java8/FunctionalInterface.java
@@ -3,6 +3,8 @@ package fr.lva.framework.java.java8;
 import java.time.Month;
 import java.time.format.TextStyle;
 import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.*;
 
 public class FunctionalInterface {
@@ -38,11 +40,25 @@ public class FunctionalInterface {
     }
 
     /**
-     * Use consumer if it takes something and return boolean
+     * Use predicate if it takes something and return boolean
      * @return Month predicates filtering "er" month
      */
     public static Predicate<Month> erMonth() {
         return m -> m.getDisplayName(TextStyle.FULL, Locale.US).endsWith("er");
+    }
+
+    /**
+     * Filter objects by unique attribute
+     *
+     * @param keyExtractor function getting attribute
+     * @param <T> object type to filter
+     * @return predicate for filtering
+     */
+    public static <T> Predicate<T> filterByAttribute(Function<? super T, ?> keyExtractor) {
+        Map<Object, Boolean> seen = new ConcurrentHashMap();
+        return (t) -> {
+            return seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+        };
     }
 
 }
--- src/test/java/fr/lva/framework/java/java8/FunctionalInterfaceTest.java
+++ src/test/java/fr/lva/framework/java/java8/FunctionalInterfaceTest.java
@@ -1,10 +1,12 @@
 package fr.lva.framework.java.java8;
 
+import fr.lva.framework.pojo.User;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.Month;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class FunctionalInterfaceTest {
@@ -31,6 +33,15 @@ public class FunctionalInterfaceTest {
         Predicate<Month> longMonth = m -> m.maxLength() == 31;
         long nbrOfLongMonthInEr = Arrays.stream(Month.values()).filter(FunctionalInterface.erMonth().and(longMonth)).count();
         Assert.assertEquals("Filters aren't ok", 2L, nbrOfLongMonthInEr);
+    }
+
+    @Test
+    public void filterByAttribute() {
+        User u1 = new User(1L, "foo","password");
+        User u2 = new User(2L, "foo","password");
+        User u3 = new User(3L, "bar","password");
+        List<User> users = Arrays.asList(u1, u2, u3);
+        Assert.assertEquals("Filtering is not ok", 2L, users.stream().filter(FunctionalInterface.filterByAttribute(User::getName)).count());
     }
 
     // Use Callable if it returns a result and might throw (most akin to Thunk in general CS terms).
